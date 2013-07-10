17325137e8b39b62c458e7ab9ac12c10316ce10a
ArtemSnake <yazynin98@gmail.com>
2013-07-10 16:37:30 +0400

    вьюшки, енум

---
+++ Gate/Models/ServerResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gate.Models
+{
+	public class ServerResponse
+	{
+		public ResponseRype ResponseRype { get; set; }
+		public object Data { get; set; }
+	}
+}
